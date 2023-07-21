d53c49a2626f05ffddeffdceeb052c03a572a849
Benyapha Soutmun <benyapha.soutmun@gmail.com>
2023-07-21 16:58:48 +0700

    adjust encourages page using vuetify card
    * decorate card with photos linked from pixabay(free license)

--- pages/encourage.vue
+++ pages/encourage.vue
@@ -1,39 +1,36 @@
 <template>
-    <div class="text-h4 text-blue-darken-4 pl-5 pt-10">Encourages  <v-btn icon="mdi-plus" size="small" to="/add-encourage"></v-btn></div>
+  <div class="text-h4 text-blue-darken-4 pl-5 pt-10">Encourages <v-btn icon="mdi-plus" size="small"
+      to="/add-encourage"></v-btn></div>
 
-    <v-carousel
-     class="pt-10"
-      cycle
-      height="500"
-      hide-delimiter-background
-      show-arrows="hover"
-    >
-      <v-carousel-item
-        v-for="({attributes}, i) in encourages.data"
-        :key="i"
-      >
-        <v-sheet
-          :color="colors[i % 5]"
-          height="100%"
-        >
-          <div class="d-flex fill-height justify-center align-center">
-            <div class="text-h2">
-              {{ attributes.message }}
-            </div>
-          </div>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
+  <v-carousel class="pt-10" cycle height="520" hide-delimiter-background show-arrows="hover">
+    <v-carousel-item v-for="({ attributes }, i) in encourages.data" :key="i">
+      <div class="ma-sm-10"><v-card>
+          <v-img :src="photos[i % photos.length]" class="align-end" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="240pt" cover>
+            <v-card-text class="text-white text-lg-h2 text-md-h3 text-sm-h5 text-xs-h7 ma-xl-10"> {{ attributes.message }}</v-card-text>
+          </v-img>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn size="small" color="red" variant="text" icon="mdi-heart"></v-btn>
+          </v-card-actions>
+        </v-card></div>
+      
+    </v-carousel-item>
+  </v-carousel>
 </template>
 <script setup lang="ts">
 import type { Encourage } from '~/types'
 const { find } = useStrapi()
 const encourages = await find<Encourage>('encourages')
-const colors = [
-    'amber-lighten-2',
-    'orange-lighten-2',
-    'deep-orange-lighten-3',
-    'pink-lighten-2',
-    'purple-lighten-3',
+const photos = [
+  'https://cdn.pixabay.com/photo/2016/02/27/18/07/clover-1225988_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2017/08/01/08/16/couple-2563424_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2017/11/26/15/16/smiley-2979107_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2015/05/11/14/51/heart-762564_640.jpg',
+  'https://cdn.pixabay.com/photo/2019/09/13/17/58/unicorns-4474543_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2017/10/23/21/13/soap-bubbles-2882599_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/03/26/22/22/happy-1281590_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2015/10/19/15/17/plush-996216_960_720.jpg',
 ]
 </script>
